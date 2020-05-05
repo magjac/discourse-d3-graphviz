1068330a12c978528731b17e4412db4be7ca33aa
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2020-05-05 11:51:17 +0200

    Add test of rendering single static graph embedded in text inline

---
+++ cypress/integration/single-static-embedded-inline.js
@@ -0,0 +1,32 @@
+describe('Inline rendering', () => {
+
+  it('renders single static graph embedded in text inline', () => {
+    cy.visit('http://localhost:3000/t/single-static-block/35');
+    cy.get('.cooked').find('text').should('have.text', 'ab');
+    cy.getCooked().then(cooked => {
+      cy.wrap(cooked).should('have.length', 1);
+      cy.wrap(cooked).findParagraphs().then(paragraphs => {
+        cy.wrap(paragraphs).should('have.length', 1);
+        cy.wrap(paragraphs).findSpans().then(spans => {
+          cy.wrap(spans).should('have.length', 3);
+        });
+        cy.wrap(paragraphs).findGraphvizContainers().then(graphvizContainers => {
+          cy.wrap(graphvizContainers).should('have.length', 1);
+          cy.wrap(graphvizContainers).findGraph().then(graph => {
+            cy.wrap(graph).should('have.length', 1);
+            cy.wrap(graph).findGraph0Group().then(graph0group => {
+              cy.wrap(graph0group).should('have.length', 1);
+              cy.wrap(graph0group).findNodes().then(nodes => {
+                cy.wrap(nodes).should('have.length', 2);
+              });
+              cy.wrap(graph0group).findEdges().then(edges => {
+                cy.wrap(edges).should('have.length', 1);
+              });
+            });
+          });
+        });
+      });
+    });
+  })
+
+})
