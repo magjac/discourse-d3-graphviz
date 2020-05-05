1fb80a354407971bbf4fd05ce3adfe3f751026ee
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2020-05-05 08:53:45 +0200

    Add Cypress commands

---
+++ cypress/support/commands.js
@@ -0,0 +1,35 @@
+Cypress.Commands.add("getCooked", () => {
+  return cy.get('.cooked');
+});
+
+Cypress.Commands.add("findParagraphs", {prevSubject: true}, (subject, index) => {
+  return cy.wrap(subject).find('> p');
+});
+
+Cypress.Commands.add("findGraphContainers", {prevSubject: true}, (subject, index) => {
+  return cy.wrap(subject).find('> span');
+});
+
+Cypress.Commands.add("findGraph", {prevSubject: true}, (subject, index) => {
+  return cy.wrap(subject).find('> svg');
+});
+
+Cypress.Commands.add("findGraph0Group", {prevSubject: true}, (subject, index) => {
+  return cy.wrap(subject).find('> #graph0');
+});
+
+Cypress.Commands.add("findNode", {prevSubject: true}, (subject, index) => {
+  return cy.wrap(subject).find('> #node' + index);
+});
+
+Cypress.Commands.add("findNodes", {prevSubject: true}, (subject, index) => {
+  return cy.wrap(subject).find('> .node');
+});
+
+Cypress.Commands.add("findEdge", {prevSubject: true}, (subject, index) => {
+  return cy.wrap(subject).find('> #edge' + index);
+});
+
+Cypress.Commands.add("findEdges", {prevSubject: true}, (subject, index) => {
+  return cy.wrap(subject).find('> .edge');
+});
