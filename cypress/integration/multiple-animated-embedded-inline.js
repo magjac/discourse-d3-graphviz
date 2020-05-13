657b4fc23f2ae3140e83ff91192430edcd47c4b3
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2020-05-13 19:57:17 +0200

    Avoid race in test of multiple animated embedded graphs

--- cypress/integration/multiple-animated-embedded-inline.js
+++ cypress/integration/multiple-animated-embedded-inline.js
@@ -1,8 +1,5 @@
 describe('Inline rendering', () => {
 
-  const topBarHeight = 60;
-  const scroll = -(topBarHeight + 10);
-
   afterEach(() => {
     cy.getStopButtons()
       .click({multiple: true, force: true });
@@ -20,50 +17,56 @@ describe('Inline rendering', () => {
     cy.visit('http://localhost:3000/t/multiple-embedded-animated-inline/37');
     cy.getCooked().then(cooked => {
       cy.wrap(cooked).should('have.length', 1);
-      cy.wrap(cooked).find('text').should('have.text', 'ababc');
       cy.wrap(cooked).findParagraphs().then(paragraphs => {
         cy.wrap(paragraphs).should('have.length', 1);
         cy.wrap(paragraphs).findSpans().then(spans => {
           cy.wrap(spans).should('have.length', 5);
           for (let i = 0; i < spanTexts.length; i++) {
-            cy.wrap(spans).eq(i).invoke('text').then(text => text.replace(/\n/g, ''))
-              .should('eq', spanTexts[i]);
+            cy.wrap(spans).eq(i).should($span => {
+              expect($span.text().replace(/\n/g, '')).to.equal(spanTexts[i]);
+            });
           }
         });
         cy.wrap(paragraphs).findGraphvizContainers().then(graphvizContainers => {
           cy.wrap(graphvizContainers).should('have.length', 2);
 
           cy.wrap(graphvizContainers).eq(0)
-            .scrollIntoView({offset: {top: scroll, left: 0}})
             .findGraph().then(graph => {
             cy.wrap(graph).should('have.length', 1);
             cy.wrap(graph).findGraph0Group().then(graph0group => {
               cy.wrap(graph0group).should('have.length', 1);
+
               cy.wrap(graph0group).findNodes().should('have.length', 2);
               cy.wrap(graph0group).findEdges().should('have.length', 1);
-              cy.wrap(graph).invoke('text').then(text => text.replace(/\n/g, ''))
-                .should('eq', 'aabba->b');
+              cy.wrap(graph).should($graph => {
+                expect($graph.text().replace(/\n/g, '')).to.equal('aabba->b');
+              });
+
               cy.wrap(graph0group).findNodes().should('have.length', 3);
               cy.wrap(graph0group).findEdges().should('have.length', 2);
-              cy.wrap(graph).invoke('text').then(text => text.replace(/\n/g, ''))
-                .should('eq', 'aabba->bcca->c');
+              cy.wrap(graph).should($graph => {
+                expect($graph.text().replace(/\n/g, '')).to.equal('aabba->bcca->c');
+              });
             });
           }).then(() => {cy.log('Done checking first graph')}); // Forces wait
 
           cy.wrap(graphvizContainers).eq(1)
-            .scrollIntoView({offset: {top: scroll, left: 0}})
             .findGraph().then(graph => {
             cy.wrap(graph).should('have.length', 1);
             cy.wrap(graph).findGraph0Group().then(graph0group => {
               cy.wrap(graph0group).should('have.length', 1);
+
               cy.wrap(graph0group).findNodes().should('have.length', 3);
               cy.wrap(graph0group).findEdges().should('have.length', 2);
-              cy.wrap(graph).invoke('text').then(text => text.replace(/\n/g, ''))
-                .should('eq', 'aabba->bcca->c');
-              cy.wrap(graph0group).findNodes().should('have.length', 3);
+              cy.wrap(graph).should($graph => {
+                expect($graph.text().replace(/\n/g, '')).to.equal('aabba->bcca->c');
+              });
+
+              cy.wrap(graph0group).findNodes().should('have.length', 4);
               cy.wrap(graph0group).findEdges().should('have.length', 3);
-              cy.wrap(graph).invoke('text').then(text => text.replace(/\n/g, ''))
-                .should('eq', 'aabba->bcca->cb->c');
+              cy.wrap(graph).should($graph => {
+                expect($graph.text().replace(/\n/g, '')).to.equal('aabba->bccb->cddd->b');
+              });
             });
           }).then(() => {cy.log('Done checking second graph')});
         });
