b3eba6826f1e4fe5b373f810b705122e2e40ecf5
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2023-12-30 16:56:04 +0100

    add creating a new topic in single static inline verbose test

--- cypress/e2e/single-static-inline-verbose.cy.js
+++ cypress/e2e/single-static-inline-verbose.cy.js
@@ -1,7 +1,10 @@
 describe('Block rendering', () => {
 
   it('renders single static graph inline verbose', () => {
-    cy.visit('http://localhost:3000/t/single-static-inline-verbose/39');
+    const title = 'Cypress testing: Single static inline verbose';
+    cy.startApplicationAndLogInAsCypressUser();
+    cy.deleteCypressTestingTopic(title);
+    cy.createNewTopic(title, '[dot verbose=true]digraph {a -> b}[/dot]');
     cy.getCooked().then(cooked => {
       cy.wrap(cooked).should('have.length', 1);
       cy.wrap(cooked).find('text').should('have.text', 'ab');
