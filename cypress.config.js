448a29278839874b18fe8f96feacc48bb55b449b
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2024-01-01 18:06:43 +0100

    configure Cypress to open chrome in a smaller window

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,14 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
+      on('before:browser:launch', (browser, launchOptions) => {
+        if (browser.name === 'chrome') {
+          launchOptions.args.push('--window-size=1540,1400'); // width set to not zoom out (100%), height never becomes more than the available height
+        }
+
+        return launchOptions;
+      });
+
       return require('./cypress/plugins/index.js')(on, config)
     },
     excludeSpecPattern: '*~',
